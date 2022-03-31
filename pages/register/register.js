c0a3ae25594915afeeca0acbe9df2ae4bec98361
verilye <connor.j.logan@outlook.com>
2022-04-01 02:54:07 +1000

    Register html and css completed

---
+++ pages/register/register.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {Storage} = require('@google-cloud/storage');
+const router = express.Router();
+const storage = new Storage();
+
+router.use(express.static(__dirname));
+
+router.get('/', async (req,res) => {
+
+      res.sendFile(__dirname + "/register.html");
+});
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const login = require('../pages/login/login')
+const register = require('../pages/register/register')
 const database = require('../pages/database/database');
 
 module.exports = function(app){
 
     app.use(express.json());
     app.use('/', login);
+    app.use('/register', register);
     app.use('/database', database);
 }
