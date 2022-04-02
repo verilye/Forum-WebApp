4c09e1a497b69d0370c9598bf09403b4c241b1ee
verilye <connor.j.logan@outlook.com>
2022-04-03 04:23:44 +1000

    First query to server through html form achieved in login.js

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,9 +1,7 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const express = require('express');
-const {Storage} = require('@google-cloud/storage');
 const router = express.Router();
-const storage = new Storage();
 
 router.use(express.static(__dirname));
 
@@ -12,18 +10,17 @@ router.get('/', async (req,res) => {
       res.sendFile(__dirname + "/login.html");
 });
 
-router.get('/:id', async (req,res)=>{
+router.get('/login', async (req,res)=>{
 
       const db = getFirestore();
 
-      const cityRef = db.collection("users").doc(req.params.id);
-      const doc = await cityRef.get();
+      const idRef = db.collection('users').doc(req.query.id);
+      const doc = await idRef.get();
       if (!doc.exists) {
       console.log('No such document!');
       } else {
       console.log('Document data:', doc.data());
       }
-
 });
 
 module.exports = router;
